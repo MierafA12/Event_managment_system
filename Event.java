a1b913162a687c0e2b93c7168b26353536f992ea
MierafA12 <mierafabebe12@gmail.com>
2025-01-17 20:10:48 +0300

    adding delete function

--- Event.java
+++ Event.java
@@ -19,21 +19,48 @@ class Admin{
     }
 
     void create_events(){
-        Scanner eventinfo = new Scanner(System.in) ; 
+        Scanner event_info = new Scanner(System.in) ; 
         System.out.println("Enter event name");
-        String eventname = eventinfo.nextLine();
+        String eventname = event_info.nextLine();
         System.out.println("Enter event date");
-        String eventdate = eventinfo.nextLine();
+        String eventdate = event_info.nextLine();
         System.out.println("Enter event time");
-        String eventtime = eventinfo.nextLine();
+        String eventtime = event_info.nextLine();
         System.out.println("Enter event location");
-        String eventlocation = eventinfo.nextLine();
-        eventinfo.nextLine();
+        String eventlocation = event_info.nextLine();
         EventDetails event = new EventDetails(eventname, eventdate, eventtime, eventlocation);
         events.add(event);
         System.out.println("Event created successfully.");
         
     };
+
+void update_events(){
+    Scanner update_info = new Scanner(System.in) ; 
+    System.out.println("Enter the event name to update");
+    String eventname = update_info.nextLine();
+ 
+}
+
+void delete_events(){
+    Scanner choose = new Scanner(System.in);
+    System.out.println("enter username");
+    String eventname = choose.nextLine();
+       for (EventDetails event : events) {
+            if (event.name.equals(eventname)) {
+                events.remove(event);
+                System.out.println("Event deleted successfully.");
+                return;
+            }
+        }
+        System.out.println("Event not found.");
+    }
+
+void display_event_registration(){
+}
+void delete_user_registration(){
+
+}
+
    void AdminLogin (){
         Scanner choose = new Scanner(System.in);
         System.out.println("enter username");
@@ -54,23 +81,37 @@ class Admin{
 
 void AdminMenu() {
     Scanner choose = new Scanner(System.in);
+    while (true){
     System.out.println("Admin Menu");
     System.out.println("1.Create event");
     System.out.println("2.Update event");
     System.out.println("3.Delete event");
     System.out.println("4.Display event and registration");
     System.out.println("5.delete user registration");
+    System.out.println("6.logout");
     System.out.println("Enter your choice from the menu");
     int choice1 = choose.nextInt();  
     switch (choice1) {
         case 1:
              create_events();
             break;
-    
+            case 2:
+            update_events();
+           break;
+           case 3:
+           delete_events();
+          break;
+          case 4:
+           display_event_registration();
+            break;
+            case 5:
+            delete_user_registration();
+            break;
       
     
-}
-}    
+        }
+   } 
+}   
 }
     
 
